594c7484da6b93c244435b8f0c4511ee3ad9576a
Late Worm <travis.rempel@gmail.com>
2018-02-20 16:12:02 -0800

    Improve descriptions

--- __tests__/repeat.js
+++ __tests__/repeat.js
@@ -1,26 +1,24 @@
 const repeat = require('../lib/repeat.js');
 
 describe('Repeat', () => {
-  describe('when given 3 consecutive matching characters', () => {
-    it('should return false', () => {
+  describe('when given a string', () => {
+    it('should return a string indicating the number of consecutive matching characters', () => {
       const result = repeat('aaabbbccc');
       expect(result).toBe('a3b3c3');
     });
   });
 
-  describe('when given 3 consecutive matching characters', () => {
-    it('should return false', () => {
+  describe('when given a string', () => {
+    it('should return a string indicating the number of consecutive matching characters', () => {
       const result = repeat('ghjff');
       expect(result).toBe('g1h1j1f2');
     });
   });
 
-	  describe('when given 3 consecutive matching characters', () => {
-    it('should return false', () => {
+  describe('when given a number', () => {
+    it('should return a string indicating the number of consecutive matching characters', () => {
       const result = repeat('3');
       expect(result).toBe('31');
     });
   });
-
-
 });
